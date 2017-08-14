77917a8cbd40fe2c119c1b2d14c84d30e12b5603
Denis Gorbach <denis.gorbach@do-it.co>
2017-08-14 11:39:43 +0300

    [MS-3]: Added bad creds label, partly localized views, added navigation from order menu.

--- www/js/controllers/SideMenuController.js
+++ www/js/controllers/SideMenuController.js
@@ -1,6 +1,12 @@
 angular.module('starter')
 
-.controller('SideMenuController', function($scope, AuthService, $ionicHistory){
+.controller('SideMenuController', function($scope, AuthService, $ionicHistory, $translate){
+  $scope.currentLang = 'de';
+
+  $scope.changeLocale = function () {
+    $scope.currentLang = $scope.currentLang === 'de' ? 'en' : 'de';
+    $translate.use($scope.currentLang);
+  };
 
   $scope.logout = function () {
     AuthService.logout();

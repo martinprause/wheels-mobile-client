ec69d7bcd98e95503f2c84024b84bef1e9e5a55d
Denis Gorbach <denis.gorbach@do-it.co>
2017-08-22 15:27:17 +0300

    [MS-3] Changed backend url.

--- www/js/auth-interceptor.js
+++ www/js/auth-interceptor.js
@@ -5,8 +5,8 @@ angular.module('starter')
     request: function (config) {
       config.headers = config.headers || {};
       if (!config.url.endsWith('.html') && !config.url.endsWith('.json')) {
-        config.url = 'http://192.168.88.97:8080' + config.url;
-        // config.url = 'http://34.202.124.10/mobile' + config.url;
+        // config.url = 'http://192.168.88.97:8080' + config.url;
+        config.url = 'http://34.202.124.10/mobile' + config.url;
         config.headers.Authorization = $window.localStorage.Authorization;
       }
       return config;
